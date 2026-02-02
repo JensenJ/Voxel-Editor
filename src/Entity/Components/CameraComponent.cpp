8295cbf3e5e225d633aadde73d949f3d052cbc19
JensenJ <13812018+JensenJ@users.noreply.github.com>
2026-02-02 02:39:32 +0000

    Try macOS action

--- src/Entity/Components/CameraComponent.cpp
+++ src/Entity/Components/CameraComponent.cpp
@@ -1,6 +1,6 @@
 #include "CameraComponent.h"
 #include <iostream>
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
 #include "../../InputManager.h"
 
 CameraComponent::CameraComponent(glm::vec3 position, float yaw, float pitch, float movementSpeed, float mouseSensitivity, float zoom)
